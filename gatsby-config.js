3531a92714d96a253983e9229933edf7bbfcfe5f
flannyan <daylennguyen@gmail.com>
2020-06-26 01:51:20 -0700

    adding netlify and gatsby plugins

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,14 +7,29 @@ module.exports = {
   pathPrefix: `/gtc`,
   plugins: [
     {
+      resolve: 'gatsby-plugin-nprogress',
+    },
+    {
       resolve: 'gatsby-plugin-manifest',
       options: {
-        name: 'Carbon Design Gatsby Theme',
-        short_name: 'Gatsby Theme Carbon',
+        name: "Calpa's Blog",
+        short_name: 'Calpa',
         start_url: '/',
         background_color: '#ffffff',
         theme_color: '#0062ff',
         display: 'browser',
+        icons: [
+          {
+            src: '/favicons/android-chrome-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: '/favicons/android-chrome-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+        ],
       },
     },
     {
@@ -27,5 +42,7 @@ module.exports = {
         },
       },
     },
+    'gatsby-plugin-offline', // put this after gatsby-plugin-manifest
+    'gatsby-plugin-netlify-cms'
   ],
 };
